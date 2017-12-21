dc3f66198b97394151054180bdf1e2bfa447421e
Alan Platt <aplatt@equalexperts.com>
2017-12-21 17:07:13 +0000

    Add entrypoint.sh to control processes required

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ A container that takes sftp uploads and deposits them in an s3 bucket.
 
 docker build --rm -t equalexpertsmicrodc/sftp-to-s3-container:latest .
 
-docker run -it -e AWSACCESSKEYID=$KEY -e AWSSECRETACCESSKEY=$SECRET_KEY --privileged equalexpertsmicrodc/sftp-to-s3-container:latest bash
+docker run -it --name sftp -p 9999:22 --privileged equalexpertsmicrodc/sftp-to-s3-container:latest --s3bucketname=my_bucket_name --sshpubkey="$(cat /Users/alanplatt/.ssh/id_rsa.pub)" --awsaccesskeyid=$KEY --awssecretaccesskey=$SECRET_KEY
 
-Inside the container you should be able to mount an s3 bucket like so:
+sftp -P 9999 -i ~/.ssh/id_rsa my_bucket_name@localhost
 
-s3fs mybucket:/ /s3bucket
 
-ls /s3bucket
+# Kubernetes deployment
+$ export NAME=sftp-to-s3
+$ kubectl apply -f <(cat k8s.yaml.template | envsubst)
---
+++ entrypoint.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+set -e
+
+err() {
+  echo -e "[ERR] ${1}"
+  exit 1
+}
+
+log() {
+  echo -e "[LOG] ${1}"
+}
+
+usage() {
+  err """
+  This container allows sftp access to an s3 bucket.
+  Please provide the required options.
+
+  REQUIRED
+  --s3bucketname           The Name of the s3 bucket only (no s3://)
+  --sshpubkey              The public key of the ssh user
+
+
+  OPTIONAL
+  --awsaccesskeyid         AWS Access Key (defaults to container profile)
+  --awssecretaccesskey     AWS Secret Access Key (as above)
+  --s3bucketpath           The path within the s3 bucket (defaults to /)
+  --sshuser                The user used for sftp (defaults to s3bucketname)
+  """
+}
+
+main() {
+
+  while [ "$1" != "" ]; do
+    PARAM="$(echo $1 | awk -F= '{print $1}')"
+    VALUE="$(echo $1 | awk -F= '{print $2}')"
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        --awsaccesskeyid)
+            OPTION_AWSACCESSKEYID="${VALUE}"
+            ;;
+        --awssecretaccesskey)
+            OPTION_AWSSECRETACCESSKEY="${VALUE}"
+            ;;
+        --s3bucketname)
+            OPTION_S3BUCKETNAME="${VALUE}"
+            ;;
+        --s3bucketpath)
+            OPTION_S3BUCKETPATH="${VALUE}"
+            ;;
+        --sshuser)
+            OPTION_SSHUSER="${VALUE}"
+            ;;
+        --sshpubkey)
+            OPTION_SSHPUBKEY="${VALUE}"
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            ;;
+    esac
+    shift
+  done
+
+  AWSACCESSKEYID="${OPTION_AWSACCESSKEYID:-unset}"
+  AWSSECRETACCESSKEY="${OPTION_AWSSECRETACCESSKEY:-unset}"
+  S3BUCKETNAME="${OPTION_S3BUCKETNAME:-unset}"
+  S3BUCKETPATH="${OPTION_S3BUCKETPATH:-/}"
+  SSHUSER="${OPTION_SSHUSER:-$S3BUCKETNAME}"
+  SSHPUBKEY="${OPTION_SSHPUBKEY:-unset}"
+
+  if [ "$S3BUCKETNAME" = "unset" ]; then
+    echo "--s3bucketname unset"
+    usage
+  fi
+
+  if [ "$SSHPUBKEY" = "unset" ]; then
+    echo "--sshpubkey unset"
+    usage
+  fi
+
+  log "Generate host keys"
+  /usr/bin/ssh-keygen -A || err "Cant generate host keys"
+
+  log "Create user ${SSHUSER}"
+  useradd -m -s /bin/false "${SSHUSER}" || err "Fail to create ${SSHUSER}"
+
+  log "Create ssh directory in user home"
+  mkdir -vp /home/${SSHUSER}/.ssh/ || err "Failed to create dir"
+
+  log "Create ssh pub key file"
+  echo "${SSHPUBKEY}" > /home/${SSHUSER}/.ssh/authorized_keys || err "Failed to create ssh pub key file"
+
+  log "Set home directory ownership and perms"
+  chmod 700 /home/${SSHUSER}/.ssh
+  chmod 600 /home/${SSHUSER}/.ssh/authorized_keys
+  chown -R ${SSHUSER}:${SSHUSER} /home/${SSHUSER}
+
+  log "Mount s3 bucket"
+  echo s3fs "${S3BUCKETNAME}:${S3BUCKETPATH}" /mnt/${S3BUCKETNAME} -f -o allow_other
+  mkdir -p /mnt/${S3BUCKETNAME}
+  s3fs "${S3BUCKETNAME}:${S3BUCKETPATH}" /mnt/${S3BUCKETNAME} -f -o allow_other &
+  S3FS_PID=$!
+
+  log "Start sshd daemon"
+  exec /usr/sbin/sshd -D -e &
+  SSHD_PID=$!
+
+  while kill -0 $S3FS_PID && kill -0 $SSHD_PID; do sleep 5; done
+
+}
+
+main "$@"
+
